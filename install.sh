0e3600bc566b197ef18f0c695f421e07f00e10d7
John Perkins <john.perkins@rackspace.com>
2013-07-17 09:14:29 -0500

    Added installer shell script (simple)

---
+++ install.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/roaet/obligate.git
+cd obligate
+mkvirtualenv obligate
+workon obligate
+mkdir lib
+cd lib
+git clone https://github.com/openstack/neutron.git
+cd neutron
+python setup.py develop
+cd ..
+git clone https://github.com/jkoelker/quark.git
+cd quark
+python setup.py develop
+cd ..
+pip install -r pip-requirements.txt
+echo 'Obligate installed ok.'
