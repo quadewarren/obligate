11b8ea20f8e0e973b6ea484c41f0563b37d10e96
mpath <john.perkins@rackspace.com>
2013-07-21 23:26:39 -0500

    Update obligate.py

--- obligate/obligate.py
+++ obligate/obligate.py
@@ -12,6 +12,8 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# TODO: Refactor print() => logging
 import netaddr
 import time
 
